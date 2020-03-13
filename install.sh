f8b93b5a79bf3319fc015ebf0cdd80438b4fc456
dokienko <sncode@yandex.ru>
2020-03-13 03:10:03 +0300

    Intermediate stage of development

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source ./vars.sh
+source vars.sh
 
 # Installing the required packages
 cat ${REQUIREMENTS_PATH}/requirements.txt | xargs sudo apt-get install
@@ -25,28 +25,28 @@ done
 
 mkdir ${LOCAL_DIR}
 
-cp ./init.sh ${LOCAL_DIR}
-cp ./vars.sh ${LOCAL_DIR}
-cp ./headers/rc-zones ${LOCAL_DIR}
+cp init.sh ${LOCAL_DIR}
+cp vars.sh ${LOCAL_DIR}
+cp headers/rc-zones ${LOCAL_DIR}
 # Add users general password
 touch ${LOCAL_DIR}/shared_password \
 && echo $OPSTKHELP_GENERAL_PASSWORD | openssl md5 -binary > ${LOCAL_DIR}/shared_password
 
 mkdir ${SUBFUNCTIONS_PATH}
-cp ./README/subfunctions ${SUBFUNCTIONS_PATH}/README
-cp ./subfiles/*\.sh ${SUBFUNCTIONS_PATH}
+cp README/subfunctions ${SUBFUNCTIONS_PATH}/README
+cp subfiles/*\.sh ${SUBFUNCTIONS_PATH}
 
 mkdir ${RC_FILES_STORAGE_PATH}
-cp ./README/rc-files ${RC_FILES_STORAGE_PATH}/README
+cp README/rc-files ${RC_FILES_STORAGE_PATH}/README
 
 mkdir ${PASSWORDS_STORAGE_PATH}
-cp ./README/rc-passwords ${PASSWORDS_STORAGE_PATH}/README
+cp README/rc-passwords ${PASSWORDS_STORAGE_PATH}/README
 
 mkdir ${SERVERS_LISTS_STORAGE_PATH}
-cp ./README/servers-lists ${SERVERS_LISTS_STORAGE_PATH}/README
+cp README/servers-lists ${SERVERS_LISTS_STORAGE_PATH}/README
 
 LOCAL_BIN_FILES_PATH=$(echo $PATH | sed 'y/:/\n/' | grep $(whoami) | sed '1d')
-cp ./opstkhelp-* $LOCAL_BIN_FILES_PATH
+cp opstkhelp-* ${LOCAL_BIN_FILES_PATH}
 
 echo "Succesfully"
 exit 0
